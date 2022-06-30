2c5d7e2f2bca4bc9183205c901aaeabb3ab9e56a
akshatk806 <106431065+akshatk806@users.noreply.github.com>
2022-07-01 00:24:20 +0530

    Adding the title

--- MagicNotes.js
+++ MagicNotes.js
@@ -5,6 +5,7 @@ showNotes();
 let addBtn=document.getElementById('addBtn');
 addBtn.addEventListener('click',function(event){
     let addTxt=document.getElementById('addTxt');
+    let addTitle=document.getElementById('addTitle');
     let notes=localStorage.getItem('notes');
     if(notes==null){
         notesObj=[];    // Empty array
@@ -12,9 +13,14 @@ addBtn.addEventListener('click',function(event){
     else{
         notesObj=JSON.parse(notes);   // convert the string to Array
     }
-    notesObj.push(addTxt.value);
+    let myObj={
+        title:addTitle.value,
+        text:addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem('notes',JSON.stringify(notesObj));  // only string is added into localStorage
-    addTxt.value=""; 
+    addTxt.value="";
+    addTitle.value=""; 
     // console.log(notesObj);
 
     showNotes(); 
@@ -24,6 +30,7 @@ addBtn.addEventListener('click',function(event){
 // Function to show elements from local storage
 function showNotes(){
     let notes=localStorage.getItem('notes');
+    console.log(notes);
     if(notes==null){
         notesObj=[];    // Empty array
     } 
@@ -34,14 +41,15 @@ function showNotes(){
     notesObj.forEach(function(element,index){
         html+=`<div class="noteCard card m-2" style="width: 18rem;">
                     <div class="card-body">
-                    <h5 class="card-title">Notes ${index+1}</h5>
-                    <p class="card-text">${element}</p>
+                    <h5 class="card-title">${element.title}}</h5>
+                    <p class="card-text">${element.text}</p>
                     <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
                 </div>`;
     }); 
 
     let notesElem=document.getElementById('notes');
+    // console.log(notesElem);
     if(notesObj.length!=0){
         notesElem.innerHTML=html;
     }
@@ -72,6 +80,7 @@ function deleteNote(index){
 let search=document.getElementById('searchTxt');
 search.addEventListener('input',function(){
     let inputVal=search.value.toLowerCase();
+    console.log(inputVal);
     // console.log("Input event fired",inputVal);
     let noteCards=document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
@@ -85,3 +94,4 @@ search.addEventListener('input',function(){
         // console.log(cardTxt);
     });
 });
+
